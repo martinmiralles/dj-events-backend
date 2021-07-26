660edbce0c298e29ef79fb702724b6efa85d8749
martinmiralles <martinmiralles66@gmail.com>
2021-07-26 15:05:52 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '355bc094a4a905308840245a7e768702'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd284b7ee-7c5a-46af-bc6d-7094b178ab50'
+};
